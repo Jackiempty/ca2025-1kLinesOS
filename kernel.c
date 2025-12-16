e30fb405181c5cb93854474f602c1700ec7f2d10
Jackiempty <candypaper0123@gmail.com>
2025-12-16 20:51:04 +0800

    feat: Complete setting of clang-format

--- kernel.c
+++ kernel.c
@@ -1,10 +1,10 @@
 #include "kernel.h"
+
 #include "common.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
 
-struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
-                       long arg5, long fid, long eid) {
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid) {
   register long a0 __asm__("a0") = arg0;
   register long a1 __asm__("a1") = arg1;
   register long a2 __asm__("a2") = arg2;
@@ -16,8 +16,7 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
 
   __asm__ __volatile__("ecall"
                        : "=r"(a0), "=r"(a1)
-                       : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
-                         "r"(a6), "r"(a7)
+                       : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
                        : "memory");
   return (struct sbiret){.error = a0, .value = a1};
 }
@@ -25,8 +24,8 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-  printf("\n\nHello %s\n", "World!");
-  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+  PANIC("booted!");
+  printf("unreachable here!\n");
 
   for (;;) {
     __asm__ __volatile__("wfi");
@@ -35,10 +34,9 @@ void kernel_main(void) {
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
   __asm__ __volatile__(
-      "mv sp, %[stack_top]\n" // Set the stack pointer
-      "j kernel_main\n"       // Jump to the kernel main function
+      "mv sp, %[stack_top]\n"  // Set the stack pointer
+      "j kernel_main\n"        // Jump to the kernel main function
       :
-      : [stack_top] "r"(
-          __stack_top) // Pass the stack top address as %[stack_top]
+      : [stack_top] "r"(__stack_top)  // Pass the stack top address as %[stack_top]
   );
 }
--- kernel.h
+++ kernel.h
@@ -5,12 +5,11 @@ struct sbiret {
   long value;
 };
 
-struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
-                       long arg5, long fid, long eid);
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid);
 
-#define PANIC(fmt, ...)                                                        \
-  do {                                                                         \
-    printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
-    while (1) {                                                                \
-    }                                                                          \
+#define PANIC(fmt, ...)                                                   \
+  do {                                                                    \
+    printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+    while (1) {                                                           \
+    }                                                                     \
   } while (0)
